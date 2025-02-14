e8b268721875c221d08d2ff789d272477e49416c
Jay <jaysawant0011@gmail.com>
2025-02-14 09:32:05 +0530

    1st Question done

--- maxusingcommandline.c
+++ maxusingcommandline.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    // Write C code here
-    printf("Try programiz.pro");
+int main(int argc, char *argv[]) {
 
+    if(argc <2){
+        printf("You havent entered anything\n");
+    }
+
+    else{
+        
+    printf("you have entered\n");
+
+    for (int i = 1; i < argc; i++)
+    {
+        printf("%d ",atoi(argv[i]));
+    }
+    
+    int max = atoi(argv[1]); // atoi converts ascii to integer
+ 
+    for (int i = 1; i < argc; i++)
+    {
+        if(atoi(argv[i]) > max){
+            max = atoi(argv[i]);
+        }  
+    }
+    printf("\nmax is %d",max);
+}
     return 0;
 }
